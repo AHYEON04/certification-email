1db72b984b2145a2326eab140a76566d49b610fd
AHYEON04 <ayeon3590@gmail.com>
2021-01-31 19:30:03 +0900

    [ADD] models/user.js
    database 연동

---
+++ models/user.js
@@ -0,0 +1,27 @@
+module.exports = (sequelize, DataTypes) => {
+    return sequelize.define("user", {
+      realname: {
+        type: DataTypes.STRING(10),
+        allowNull: false,
+      },
+      nickname: {
+        type: DataTypes.STRING(20),
+        allowNull: false,
+        unique: true,
+      },
+      id: {
+        type: DataTypes.STRING(15),
+        allowNull: false,
+        primaryKey: true,
+        unique: true,
+      },
+      password: {
+        type: DataTypes.STRING(20),
+        allowNull: false,
+      },
+      pnumber: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+    });
+  };
