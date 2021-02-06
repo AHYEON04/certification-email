119ae5ed84c6a01b34aa0da25c11ac1c5b5dd67d
최아연 <201120cay@dsm.hs.kr>
2021-02-06 23:28:48 +0900

    Update user.js

--- controller/user.js
+++ controller/user.js
@@ -1,7 +1,1 @@
-const {User, sequelize} = require("../models");
-const jwt = reuqire("jsonwebtoken");
 
-const signup = async(req, res)=>{
-    const {userId, password, realname, nickname, pnumber} = req.body;
-    
-}
