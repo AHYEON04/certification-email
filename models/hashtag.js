31da67152db3d86aa7a9abd402437b82eca558e9
AHYEON04 <ayeon3590@gmail.com>
2021-06-02 22:56:53 +0900

    Add models/hashtag

---
+++ models/hashtag.js
@@ -0,0 +1,9 @@
+const Sequelize = require('sequelize');
+
+module.exports = class Hashtag extends Sequelize.Model{
+    static init(sequelize){
+        return super.init({
+            
+        })
+    }
+}
