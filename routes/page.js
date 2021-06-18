cf68c959ce5cedf9ee16487f24f4e2287d103e17
AHYEON04 <ayeon3590@gmail.com>
2021-06-18 13:08:25 +0900

    Update routes/page

--- routes/page.js
+++ routes/page.js
@@ -2,28 +2,28 @@ const express = require('express');
 
 const router = express.Router();
 
-router.use((req, res, next)=>{
-    res.locals.user = null;
-    res.locals.followCount = 0;
-    res.locals.followingCount = 0;
-    res.locals.followerIdList = [];
-    next();
+router.use((req, res, next) => {
+  res.locals.user = null;
+  res.locals.followerCount = 0;
+  res.locals.followingCount = 0;
+  res.locals.followerIdList = [];
+  next();
 });
 
-router.get('/profile',(req, res)=>{
-    res.render('profile',{title: '내 정보'});
-})
-
-router.get('/join',(req, res)=>{
-    res.render('join',{title:'회원가입'});
+router.get('/profile', (req, res) => {
+  res.render('profile', { title: '내 정보 - NodeBird' });
 });
 
-router.get('/',(req, res)=>{
-    const twits =[];
-    res.render('main',{
-        title:'',
-        twits, 
-    });
+router.get('/join', (req, res) => {
+  res.render('join', { title: '회원가입 - NodeBird' });
 });
 
-modules.exprot = router;
+router.get('/', (req, res, next) => {
+  const twits = [];
+  res.render('main', {
+    title: 'NodeBird',
+    twits,
+  });
+});
+
+module.exports = router;
